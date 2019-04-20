5e80af012be4fc678610b46bc7a9f718f733be38
pch91 <pch91@bol.com.br>
2019-04-20 03:26:12 -0300

    commit inicial de classes bases

--- app/src/main/java/com/mequa/mequasovsoft/MODAL/Medicao.java
+++ app/src/main/java/com/mequa/mequasovsoft/MODAL/Medicao.java
@@ -1,23 +1,60 @@
 package com.mequa.mequasovsoft.MODAL;
 
+import com.google.firebase.database.IgnoreExtraProperties;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+
+@IgnoreExtraProperties
 public class Medicao {
 
     @SerializedName("created_at")
     @Expose
-    private String createdAt;
+    public int temperatura;
+
     @SerializedName("description")
     @Expose
-    private String description;
+    public double umidade;
+
     @SerializedName("done")
     @Expose
-    private Boolean done;
+    public Boolean chuvendo;
+
     @SerializedName("id")
     @Expose
-    private String id;
+    public double umidadear;
 
+    public int getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(int temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public double getUmidade() {
+        return umidade;
+    }
+
+    public void setUmidade(double umidade) {
+        this.umidade = umidade;
+    }
+
+    public Boolean getChuvendo() {
+        return chuvendo;
+    }
+
+    public void setChuvendo(Boolean chuvendo) {
+        this.chuvendo = chuvendo;
+    }
+
+    public double getUmidadear() {
+        return umidadear;
+    }
+
+    public void setUmidadear(double umidadear) {
+        this.umidadear = umidadear;
+    }
 }
